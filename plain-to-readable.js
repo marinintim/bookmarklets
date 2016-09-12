ffdc1b20b90e1d2d853b6dbcc429332d80f839bf
Tim Marinin <mt@marinin.xyz>
2016-09-12 11:48:38 -0400

    feat: add plain-to-readable

---
+++ plain-to-readable.js
@@ -0,0 +1,9 @@
+/// plain-to-readable.js
+
+!(function plainToReadable() {
+  var body = document.body
+  body.style.maxWidth = '800px'
+  body.style.fontSize = '14pt'
+  body.style.lineHeight = '1.618'
+  body.style.fontFamily = 'Georgia, serif, Arial'
+})()
